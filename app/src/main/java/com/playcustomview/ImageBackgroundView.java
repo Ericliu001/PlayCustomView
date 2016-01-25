5abe1155977641abb9f864d5d2467eb9304727ec
Eric Liu <eric.liu.developer@gmail.com>
2016-01-26 00:22:35 +1100

    it works

--- app/src/main/java/com/playcustomview/ImageBackgroundView.java
+++ app/src/main/java/com/playcustomview/ImageBackgroundView.java
@@ -2,13 +2,14 @@ package com.playcustomview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by ericliu on 25/01/2016.
  */
-public class ImageBackgroundView extends View {
+public class ImageBackgroundView extends ImageView {
     public ImageBackgroundView(Context context) {
         this(context, null);
     }
@@ -20,13 +21,16 @@ public class ImageBackgroundView extends View {
     public ImageBackgroundView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        TypedArray a = context.getTheme().obtainStyledAttributes(
+        TypedArray a = context.obtainStyledAttributes(
                 attrs,
-                R.styleable.PieChart,
+                R.styleable.ImageBackgroundView,
                 0, 0);
 
         try {
-            mShowText = a.getRe(R.stpyleable.ImageBackgroundView_backgroundImageå);
+            Drawable drawable = a.getDrawable(R.styleable.ImageBackgroundView_backgroundImage);
+            if (drawable != null) {
+                this.setImageDrawable(drawable);
+            }
         } finally {
             a.recycle();
         }
